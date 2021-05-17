e15935c25de0b10e6d79aa34f9d90ac75d4f2974
Shradha1824 <83284807+Shradha1824@users.noreply.github.com>
2021-05-16 21:59:28 -1200

    Update EmpWageComputation.java

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,10 +2,10 @@ public class EmpWageComputation {
      public static void main(String[] args) {
 	 System.out.println("Welcome to Employee Wage Computation Program");
              //Constants
-              int IS_FULL_TIME = 1;
+              int ATTENDANCE = 1;
              //Computation
               double empCheck = Math.floor(Math.random() * 10) % 2;
-              if (empCheck == IS_FULL_TIME)
+              if (empCheck == ATTENDANCE)
                     System.out.println("Employee is Present");
               else
                     System.out.println("Employee is Abscent");
